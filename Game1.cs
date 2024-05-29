e3205f256e292d70c156b52f9e5c0489a4d63fb0
Naqi Master <701018804@GoTVDSB.ca>
2024-05-29 14:43:10 -0400

    working on map

--- Game1.cs
+++ Game1.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 
 namespace FInal_Summative
 {
@@ -29,14 +30,16 @@ namespace FInal_Summative
             base.Initialize();
 
             barriers = new List<Rectangle>();
+            barriersAdd();
+
 
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-            player = new Player(Content.Load<Texture2D>("marioTextureStand"), new Vector2(50, 50));
-            barrierTexture = Content.Load<Texture2D>("Brick1");
+            player = new Player(Content.Load<Texture2D>("Brick1"), new Vector2(50, 50));
+            barrierTexture = Content.Load<Texture2D>("gold");
 
             // TODO: use this.Content to load your game content here
         }
@@ -45,9 +48,8 @@ namespace FInal_Summative
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-
+            this.Window.Title = "Number of barriers: " + barriers.Count;
             player.Update(gameTime,barriers);
-            barriersAdd();
 
 
 
@@ -59,8 +61,27 @@ namespace FInal_Summative
 
         public void barriersAdd()
         {
-            barriers.Add(new Rectangle(0, 390,600,10));
+            //3rd Floor
+            barriers.Add(new Rectangle(0, 250, 50, 10));
+            barriers.Add(new Rectangle(72, 250, 622, 10));
+            barriers.Add(new Rectangle(750, 250, 150, 10));
+            barriers.Add(new Rectangle(500, 170, 10, 80));
+
+
+
+            //Block in between 2nd and 3rd floor
+            barriers.Add(new Rectangle(50, 310, 22, 10));
+            barriers.Add(new Rectangle(770, 310, 30, 10));
+
+
+            //2nd Floor
+            barriers.Add(new Rectangle(0, 370,700,10));
+
+            //1st Floor
             barriers.Add(new Rectangle(0, 450, 800, 10));
+
+            //Box on Floor
+            barriers.Add(new Rectangle(780, 430, 20, 20));
         }
 
         protected override void Draw(GameTime gameTime)
@@ -71,7 +92,7 @@ namespace FInal_Summative
             player.Draw(_spriteBatch);
 
             foreach (Rectangle barrier in barriers)
-                _spriteBatch.Draw(barrierTexture, barrier, Color.Black);
+                _spriteBatch.Draw(barrierTexture, barrier, Color.White);
 
             _spriteBatch.End();
 
